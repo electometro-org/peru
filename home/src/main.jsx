fe60d05619473cef145402d02d13f4017e63db68
seffs <46768740+seffs@users.noreply.github.com>
2026-02-06 00:17:30 +0100

    feat: loading screen animation

--- home/src/main.jsx
+++ home/src/main.jsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { TolgeeProvider } from "@tolgee/react";
 import { tolgee } from './tolgee.js'
 import App from './App.jsx'
+import LoadingScreen, { LoadingWrapper } from './components/LoadingScreen.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <TolgeeProvider
-    tolgee={tolgee}
-    fallback="Cargando..."
-  >
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </TolgeeProvider>,
+    <TolgeeProvider
+        tolgee={tolgee}
+        fallback={<LoadingScreen />}
+    >
+      <LoadingWrapper minDelay={500}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </LoadingWrapper>
+    </TolgeeProvider>,
 )
