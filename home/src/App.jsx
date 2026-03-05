f37050d4a70a9cb0315365a29e9197d6797bb53b
seffs <46768740+seffs@users.noreply.github.com>
2026-03-05 10:58:33 +0100

    fix: replace wrong path with root path

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import ScrollToTop from './components/ScrollToTop.jsx'
 import Nav from './components/Nav.jsx'
 import Home from './pages/Home.jsx'
+
+function HomeWithRedirect() {
+  useEffect(() => {
+    window.history.replaceState(null, '', '/')
+  }, [])
+  return <Home />
+}
 import AttributionsPage from './pages/AttributionsPage.jsx'
 import Footer from './components/Footer.jsx'
 
@@ -15,7 +23,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/atribuciones" element={<AttributionsPage />} />
-          <Route path="*" element={<Home />} />
+          <Route path="*" element={<HomeWithRedirect />} />
         </Routes>
         <Footer />
       </div>
