8011c1cf1abfb7f6d69c2fb783d3756de4cb066c
seffs <46768740+seffs@users.noreply.github.com>
2026-03-05 09:53:58 +0100

    fix: open electometro URLs within same tab

--- home/src/components/Footer.jsx
+++ home/src/components/Footer.jsx
@@ -29,9 +29,9 @@ function Footer() {
             </h4>
             <ul className="footer-links">
               <li><Link to="/atribuciones"><T keyName="footer.info.attributions">Atribuciones</T></Link></li>
-              <li><a target="_blank" href={`${import.meta.env.VITE_ELECTOMETRO_URL}/#/politica-privacidad`}><T keyName="footer.info.privacy">Política de Privacidad</T></a></li>
-              <li><a target="_blank" href={`${import.meta.env.VITE_ELECTOMETRO_URL}/#/metodologia`}><T keyName="footer.info.methodology">Metodología</T></a></li>
-              <li><a target="_blank" href={`${import.meta.env.VITE_ELECTOMETRO_URL}/#/contacto`}><T keyName="footer.info.contact">Contacto</T></a></li>
+              <li><a href={`${import.meta.env.VITE_ELECTOMETRO_URL}/#/politica-privacidad`}><T keyName="footer.info.privacy">Política de Privacidad</T></a></li>
+              <li><a href={`${import.meta.env.VITE_ELECTOMETRO_URL}/#/metodologia`}><T keyName="footer.info.methodology">Metodología</T></a></li>
+              <li><a href={`${import.meta.env.VITE_ELECTOMETRO_URL}/#/contacto`}><T keyName="footer.info.contact">Contacto</T></a></li>
             </ul>
           </div>
         </div>
