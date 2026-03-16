65e2b8761d3d2976f055d4d39fd47e2750b0ffb0
seffs <46768740+seffs@users.noreply.github.com>
2026-03-16 04:41:52 +0100

    fix: infinite loading screen on attributions view

--- home/src/pages/AttributionsPage.jsx
+++ home/src/pages/AttributionsPage.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react'
 import Attributions from '../components/Attributions'
+import { useLoading } from '../components/LoadingScreen'
 
 function AttributionsPage() {
+  const { setCarouselReady } = useLoading()
+
+  useEffect(() => {
+    setCarouselReady(true)
+  }, [setCarouselReady])
+
   return (
     <div style={{ paddingTop: '80px' }}>
       <Attributions />
